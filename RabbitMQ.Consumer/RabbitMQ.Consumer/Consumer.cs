984cd04c79639492025f2acd3b491998e40b4cb7
Forhad Khan <forhad.hussain@asabd.org>
2022-08-10 11:16:53 +0600

    RabbitMQ Using Docker Image

---
+++ RabbitMQ.Consumer/RabbitMQ.Consumer/Consumer.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQ.Consumer
+{
+    public static class Consumer
+    {
+        public static void Consume(IModel channel)
+        {
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (sender, e) =>
+            {
+                var body = e.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(message);
+            };
+
+            channel.BasicConsume("demo-queue", true, consumer);
+            Console.WriteLine("Consumer Startted.");
+            Console.ReadLine();
+        }
+    }
+}
